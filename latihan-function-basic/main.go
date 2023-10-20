97750c88ad45931a0a5b2c11e7119122a7de84dc
ekokurniawann <ekokurniawaann@gmail.com>
2023-10-20 13:45:31 +0700

    quiz-sesi-5

---
+++ latihan-function-basic/main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+type Car struct {
+	data map[string]string
+}
+
+
+// Cara pertama dengan menggunakan dua fungsi
+func prosesCar(car map[string]string) string {
+	name := car["name"]
+	color := car["color"]
+	result := fmt.Sprintf("Mobil %s Berwarna %s\n", name, color)
+	return result
+}
+
+func printCar(message string) {
+	fmt.Println(message)
+}
+
+// Cara kedua menggunakan satu fungsi
+func prosesCar1() string {
+	car := make(map[string]string)
+	car["name"] = "BMW"
+	car["color"] = "Black"
+	deskripsi := fmt.Sprintf("Mobil %s Berwarna %s\n", car["name"], car["color"])
+	return deskripsi
+}
+
+// Cara ketiga menggunakan anonymous function
+var printCar2 = func() string {
+	car := make(map[string]string)
+	car["name"] = "BMW"
+	car["color"] = "Black"
+	deskripsi := fmt.Sprintf("Mobil %s Berwarna %s\n", car["name"], car["color"])
+	return deskripsi
+}
+
+// Cara ke empat kita membuat method
+func (c *Car) printCar3() string {
+	name := c.data["name"]
+	color := c.data["color"]
+	deskripsi := fmt.Sprintf("Mobil %s Berwarna %s\n", name, color)
+	return deskripsi
+	
+}
+
+func main() {
+	car := map[string]string{
+		"name": "BMW",
+		"color": "Black",
+	}
+
+	// Mencetak Cara pertama
+	message := prosesCar(car)
+	printCar(message)
+
+	// Mencetak Cara kedua
+	message1 := prosesCar1()
+	fmt.Println(message1)
+
+	// Mencetak Cara ketiga
+	message2 := printCar2()
+	fmt.Println(message2)
+
+	// Cara ke empat 
+	// Kita implementasi struct
+	car1 := Car{data: map[string]string{"name": "BMW", "color": "Black"}}
+	
+	message3 :=car1.printCar3()
+	fmt.Println(message3)
+}
